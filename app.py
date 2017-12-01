0449ae270cd493fbc5def75a852b12ed95828495
Adrian Sypos <adrian00935@gmail.com>
2017-12-01 16:31:03 +0000

    parsing image from canvas and calling it from a request at /predict/

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+	
+@app.route('/predict/', methods=['GET','POST'])
+def predict():
+    # get data from drawing canvas and save as image
+    parseImage(request.get_data())
 
+def parseImage(imgData):
+    # parse canvas bytes and save as output.png
+    imgstr = re.search(b'base64,(.*)', imgData).group(1)
+    with open('output.png','wb') as output:
+        output.write(base64.decodebytes(imgstr))
+	
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
